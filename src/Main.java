99e31bcc672aa179dd0e91c5f6bfc0024f724378
AleDerosa1 <derosaalessandro018@gmail.com>
2025-05-03 16:15:23 +0200

    da continuare

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,14 @@ import org.lessons.java.shop.Prodotto;
 public class Main {
     public static void main(String[] args) {
      
+        Prodotto gioco = new Prodotto("FC24", "Per PlayStation 4", 79.9f, 22);
+        System.out.println("Codice: " + gioco.getCodice());
+        System.out.println("Nome Gioco: " + gioco.getNome());
+        System.out.println("Descrizione: " + gioco.getDescrizione());
+        System.out.println("Prezzo: " + gioco.getPrezzoBase() + "$");
+        System.out.println("Prezzo con IVA: " + gioco.getPrezzoIva() + "$");
+
+        System.out.println(gioco.getNomeCodice());
         
 
     }
--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -7,34 +7,34 @@ public class Prodotto {
     public int codice;
     public String nome;
     public String descrizione;
-    public float prezzo;
+    public float prezzobase;
     public float iva;
 
-    public Prodotto (String nome, String descrizione, float prezzo, int iva){
+    public Prodotto (String nome, String descrizione, float prezzobase, int iva){
         Random numero = new Random();
         this.codice = numero.nextInt(1000);
         this.nome = nome;
         this.descrizione = descrizione;
-        this.prezzo = prezzo;
+        this.prezzobase = prezzobase;
         this.iva = 22;
     }
 
     //METODO PER OTTENRE PREZZO BASE
-    public float getPrezzo(){
-        return prezzo;
+    public float getPrezzoBase(){
+        return prezzobase;
     }
 
-    public void setPrezzo(float prezzo) {
-        this.prezzo = prezzo;
+    public void setPrezzoBase(float prezzobase) {
+        this.prezzobase = prezzobase;
     }
 
     //METODO PER OTTENERE PREZZO CON IVA
     public float getPrezzoIva(){
-        return prezzo + (prezzo * iva / 100);
+        return prezzobase + (prezzobase * iva / 100);
     }
 
 
-    public void setIva(int iva) {
+    public void setPrezzoIva(int iva) {
         this.iva = iva;
     }
 
@@ -43,21 +43,27 @@ public class Prodotto {
         return nome + "-" + codice;
     }
 
+
     public int getCodice() {
         return codice;
     }
+
+
 
     public String getNome() {
         return nome;
     }
 
+
     public void setNome(String nome) {
         this.nome = nome;
     }
 
+
     public String getDescrizione() {
         return descrizione;
     }
+
 
     public void setDescrizione(String descrizione) {
         this.descrizione = descrizione;
